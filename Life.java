c35dc74832d43d9d0c1fab3521459d3d9ec870dc
An Tran <an.tran2131@gmail.com>
2015-11-01 13:06:14 -0500

    Added JFrame instance to display game board instead of outputing to terminal.

--- Life.java
+++ Life.java
@@ -1,27 +1,37 @@
 /*
 The main game
 */
+import javax.swing.JFrame;
+import java.awt.GridLayout;
 
 public class Life{
     public static void main(String [] args){
-        // game runs for 10 generations with 0.2 second delay between generations
-        GameControl game = new GameControl(10, 200);
+        // game runs for 10 generations with 0.5 second delay between generations
+        GameControl game = new GameControl(10, 500);
 
         // init game board
         Cell [][] board = game.makeBoard(10, 10);
+        
+        // make game window
+        JFrame f = new JFrame("Life");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GridLayout gameLayout = new GridLayout(board.length, board.length);
+        f.setLayout(gameLayout);
+        f.pack();
+        f.setVisible(true);       
         
         // insert glider at position (2, 2)
         game.insertGlider(board, 2, 2);
         
         // print game board
-        game.printBoard(board);
+        game.printBoard(board, f);
 
         int gameIterations = game.getMaxGenerations();
 
         for(int i = 1; i <= gameIterations; i++){
             System.out.println("Generation: "+i+"/"+gameIterations);
             game.evaluateBoard(board);
-            game.printBoard(board);
+            game.printBoard(board, f);
             game.delayGame(game.getDelay());
         }
     }
