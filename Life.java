c2e20f869929b6226a7e27fef44f7e6a71ac702d
An Tran <an.tran2131@gmail.com>
2015-11-09 00:01:25 -0500

    Changed iteration counter to zero based count.

--- Life.java
+++ Life.java
@@ -7,7 +7,7 @@ import java.awt.GridLayout;
 public class Life{
     public static void main(String [] args){
         // game runs for 10 generations with 1 second delay between generations
-        GameControl game = new GameControl(3);
+        GameControl game = new GameControl(10);
 
         // init game board
         Cell [][] board = game.makeBoard(10, 10);
@@ -25,8 +25,8 @@ public class Life{
         int gameIterations = game.getMaxGenerations();
 
         // iterate through every generation
-        for(int i = 1; i <= gameIterations; i++){
-            System.out.println("Generation: "+i+"/"+gameIterations);
+        for(int i = 0; i < gameIterations; i++){
+            System.out.println("Evaluating Generation: "+i);
             board = game.evaluateBoard(board); // current board takes on new values for next generation
             game.printBoard(board, display);
             game.delayGame(game.getDelay());
